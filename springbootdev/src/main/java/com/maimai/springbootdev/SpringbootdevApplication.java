9aff9db8e86a9b6e8a5ffbdbe36a167ab058babf
baicaocanhua <330557864@qq.com>
2020-01-03 10:28:24 +0800

    1

---
+++ springbootdev/src/main/java/com/maimai/springbootdev/SpringbootdevApplication.java
@@ -0,0 +1,13 @@
+package com.maimai.springbootdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdevApplication.class, args);
+	}
+
+}
