39591c4b58e03cf894f32327039fbff2dc09c657
baicaocanhua <330557864@qq.com>
2020-01-03 19:43:51 +0800

    1

--- springbootdev/src/main/java/com/maimai/springbootdev/SpringbootdevApplication.java
+++ springbootdev/src/main/java/com/maimai/springbootdev/SpringbootdevApplication.java
@@ -2,12 +2,20 @@ package com.maimai.springbootdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootdevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdevApplication.class, args);
 	}
+
+    @RequestMapping("/")
+    public String index(){
+        return "欢迎访问 springboot-aop";
+    }
 
 }
